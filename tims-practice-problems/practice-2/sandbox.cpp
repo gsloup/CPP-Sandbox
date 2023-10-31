f40f69c1c2a18e6538d2c16ffd8c2940649ac462
Garrett Sloup <garrettsloup@gmail.com>
2023-10-30 21:22:37 -0500

    plays with heap and stack some more

--- tims-practice-problems/practice-2/sandbox.cpp
+++ tims-practice-problems/practice-2/sandbox.cpp
@@ -12,10 +12,32 @@ int main() {
     
     string* hairColor; // Declare a pointer on the stack
 
+    /*-------------------------------------------------------------------*/
+
     hairColor = new string("Brown"); // Allocate a string on the heap and assign the pointer
 
     cout << "Heap: My hair is " << *hairColor << ", what is yours?" << endl;
     cout << "Heap: This is the memory address of 'hairColor': " << hairColor << endl;
 
     delete hairColor; // Free the memory on the heap
+
+    /*-------------------------------------------------------------------*/
+
+    int age1; // stack
+    int* age2; // stored on stack, points to an int on heap
+
+    age1 = 18;
+
+    age2 = &age1; // assigns age2 to the address of age1
+
+    cout << "The stack age is " << age1 << endl;
+    cout << "The heap pointed age is " << *age2 << endl; // value of age2
+
+    *age2 = 21;
+
+    cout << "If I change the value of '*age2', it will affect 'age1' since it points to the same address in memory." << endl;
+    cout << "'*age2'= " << *age2 << " and 'age1'= " << age1 << endl;
+
+    /*-------------------------------------------------------------------*/
+
 }
