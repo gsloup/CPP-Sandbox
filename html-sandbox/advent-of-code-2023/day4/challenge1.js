ba8a0c9b3311a4fcd30a054cb7558fdd290a6017
Garrett Sloup <garrettsloup@gmail.com>
2023-12-10 23:49:01 -0600

    more changes

--- html-sandbox/advent-of-code-2023/day4/challenge1.js
+++ html-sandbox/advent-of-code-2023/day4/challenge1.js
@@ -23,24 +23,40 @@ So, in this example, the Elf's pile of scratchcards is worth 13 points.
 Take a seat in the large pile of colorful cards. How many points are they worth in total?
 */
 
-const fs = require("fs");
-const input = fs
-  .readFileSync("html-sandbox/advent-of-code-2023/day4/input.txt")
-  .toString()
-  .split("\n");
+// const fs = require("fs");
+// const input = fs
+//   .readFileSync("html-sandbox/advent-of-code-2023/day4/input.txt")
+//   .toString()
+//   .split("\n");
 
 // console.log(input[0]);
 
-function processCardData(stringArray) {
-  for (let line of stringArray) {
-    let [card, winningNums, actualNums] = line.split(/[:|]/);
+// EXAMPLE
+let input = [
+  "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+  "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+  "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+  "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+  "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+  "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+];
 
-    card.split(" ");
-    let cardNum = card.length - 1;
-    console.log("Card", cardNum);
+const processCardData = (input) => {
+  for (let line of input) {
+    let [card, winningNums, actualNums] = line.split(/[:|]/);
+    console.log("Card", card);
+    winningNums = winningNums.split(/\s+/).map(Number);
+    actualNums = actualNums.split(/\s+/).map(Number);
+
+    for (let num of actualNums) {
+      if (winningNums.includes(num)) {
+        console.log("Match: ", actualNums[num]);
+      }
+    }
+
     // console.log("Winning Nums", winningNums);
     // console.log("Actual Nums", actualNums);
   }
-}
+};
 
 processCardData(input);
